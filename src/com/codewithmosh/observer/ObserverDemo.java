a72885bf19728e86869175eb8d3db3946ded7164
AmanDeep-21 <amandeepsmile21@gmail.com>
2023-05-18 11:04:43 +0530

    Observer Pattern - Pull Style

--- src/com/codewithmosh/observer/ObserverDemo.java
+++ src/com/codewithmosh/observer/ObserverDemo.java
@@ -4,9 +4,9 @@ public class ObserverDemo {
 
     public void execute() {
         var dataSource = new DataSource();
-        var sheet1 = new SpreadSheet();
-        var sheet2 = new SpreadSheet();
-        var chart = new Chart();
+        var sheet1 = new SpreadSheet(dataSource);
+        var sheet2 = new SpreadSheet(dataSource);
+        var chart = new Chart(dataSource);
 
         dataSource.addObserver(sheet1);
         dataSource.addObserver(sheet2);
