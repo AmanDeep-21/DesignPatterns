8bf015759d90df2a874a73cd0e0369f514cede2b
AmanDeep-21 <amandeepsmile21@gmail.com>
2023-05-18 10:53:55 +0530

    Observer Pattern - Push Style

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+import com.codewithmosh.observer.ObserverDemo;
 import com.codewithmosh.strategy.StrategyDemo;
 
 public class Main {
     public static void main(String[] args) {
 
         // Strategy Demo
-        StrategyDemo strategyDemo = new StrategyDemo();
-        strategyDemo.execute();
+//        StrategyDemo strategyDemo = new StrategyDemo();
+//        strategyDemo.execute();
 
+        // Observer or Pub-Sub Demo
+        ObserverDemo observerDemo = new ObserverDemo();
+        observerDemo.execute();
     }
 }
---
+++ src/com/codewithmosh/observer/Chart.java
@@ -0,0 +1,8 @@
+package com.codewithmosh.observer;
+
+public class Chart implements Observer {
+    @Override
+    public void update(int value) {
+        System.out.println("Chart got updated : " + value);
+    }
+}
